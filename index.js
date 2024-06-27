df47f83a6d6b8e8a18002b2c9261a55686c773b8
mehranbeikahmadi <51816478+mehranbeikahmadi@users.noreply.github.com>
2024-06-27 10:10:52 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const display = document.getElementById('mehran');
+function appendToDisplay(input) {
+    display.value += input;
+
+}
+function clearDisplay() {
+    display.value = '';
+}
+function calculate() {
+    try {
+        display.value = eval(display.value);
+
+    } catch (error) {
+        display.value = "error";
+    }
+}
